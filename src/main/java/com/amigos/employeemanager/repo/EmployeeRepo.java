2d887ded9477dabae3ec0da835bee5bd082b5e14
Daria Supińska <dariasupinska@gmail.com>
2021-02-19 00:59:22 +0100

    EmployeeRepo adjusted and EmployeeService added

--- src/main/java/com/amigos/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/amigos/employeemanager/repo/EmployeeRepo.java
@@ -3,5 +3,10 @@ package com.amigos.employeemanager.repo;
 import com.amigos.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
 }
