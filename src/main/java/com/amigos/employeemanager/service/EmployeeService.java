7fd79e35e3a8d9badf2850bfb2ec82e8d12c0dbd
Daria Supińska <dariasupinska@gmail.com>
2021-02-19 01:02:54 +0100

    exception handling

--- src/main/java/com/amigos/employeemanager/service/EmployeeService.java
+++ src/main/java/com/amigos/employeemanager/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.amigos.employeemanager.service;
 
+import com.amigos.employeemanager.exception.UserNotFoundException;
 import com.amigos.employeemanager.model.Employee;
 import com.amigos.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +32,8 @@ public class EmployeeService {
     }
 
     public Employee findEmployeeById(Long id) {
-        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id "+id+" was not found"));
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id "+id+" was not found"));
     }
 
     public void deleteEmployee(Long id){
